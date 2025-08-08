b98416be7c5f8e35f889c37dbaabec248b0bbc4a
romiebanerjee <romie.banerjee@gmail.com>
2025-08-08 13:58:26 +0200

    remove __init__.py

--- laplace/__init__.py
+++
@@ -1,4 +0,0 @@
-from kfac.kfac import KFAC
-from kfac.fisher_tools import kf_inner, invert_cholesky, invert_fisher, kf_eigens
-
-__all__ = ["KFAC", "kf_inner", "invert_cholesky", "invert_fisher", "kf_eigens"]
